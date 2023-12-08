571a8d0c07aafc1345a2531532e8a5d08efcfe83
PBKoning <pbkoning@gmail.com>
2023-12-08 20:58:40 +0100

    More code added

--- Object-Oriented Python - Master OOP by Building Games and GUIs/Chapter 5/02 Drawing an image/demo 4b - OneBallBounceWithSound.py
+++ Object-Oriented Python - Master OOP by Building Games and GUIs/Chapter 5/02 Drawing an image/demo 4b - OneBallBounceWithSound.py
@@ -22,7 +22,9 @@ clock = pygame.time.Clock()
 
 # 4 - Load assets: image(s), sound(s), etc.
 ballImage = pygame.image.load(IMAGES_PATH+'ball.png')
-bounceSound = pygame.mixer.Sound(SOUNDS_PATH + 'boing.wav')
+bounceSound = pygame.mixer.Sound(SOUNDS_PATH+'boing.wav')
+pygame.mixer.music.load(SOUNDS_PATH+'background.mp3')
+pygame.mixer.music.play(-1, 0.0)
 
 # 5 - Initialize variables
 ballRect = ballImage.get_rect()
